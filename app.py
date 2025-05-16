d6e72f01ba3c4fcc489a52a9f37e0df1b961b4fa
Mahmoud Adel <86129182+M2hmoud2del@users.noreply.github.com>
2025-05-16 12:44:28 +0300

    Update app.py
    Add enviroment variable to Code for security issues

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask_mail import Mail, Message
 from flask import Flask
+from flask_mail import Mail, Message
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+# Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
 
 def send_email(recipient_email, subject, message_body):
-    app = Flask(__name__)
-
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = "" 
-    app.config['MAIL_PASSWORD'] = ""
-    app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
-
-    mail = Mail(app)
-
     with app.app_context():
         msg = Message(
             subject=subject,
@@ -22,3 +27,9 @@ def send_email(recipient_email, subject, message_body):
         )
         mail.send(msg)
         print(f"Email sent to {recipient_email}")
+
+if __name__ == "__main__":
+    recipient = input("Recipient Email: ")
+    subject = input("Subject: ")
+    body = input("Message Body: ")
+    send_email(recipient, subject, body)
